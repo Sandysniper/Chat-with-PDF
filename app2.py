e70014102f1f1d2f24e229b1cb530e3d703a8698
Sandysniper <156170528+Sandysniper@users.noreply.github.com>
2024-02-24 21:13:20 +0530

    Add files via upload

---
+++ app2.py
@@ -0,0 +1,109 @@
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import os
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import google.generativeai as genai
+from langchain.vectorstores.chroma import Chroma 
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+
+
+
+
+
+def get_pdf_text(pdf_docs):
+    text=""
+    for pdf in pdf_docs:
+        pdf_reader= PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text+= page.extract_text()
+    return  text
+
+
+
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+
+def get_vector_store(text_chunks):
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+    vector_store = Chroma(embedding_function=embeddings, collection_name="pdf_collection" ,persist_directory="db")
+    vector_store.add_texts(text_chunks)
+    vector_store.persist()
+    
+
+
+def get_conversational_chain():
+
+    prompt_template = """
+    Answer the question as detailed as possible from the provided context, make sure to provide all the details,search as much as possible understand the Question well and gather all the information about that in the provided context and provide the correct answer but don't provide the wrong answer\n\n
+    Context:\n {context}?\n
+    Question: \n{question}\n
+
+    Answer:
+    """
+
+    model = ChatGoogleGenerativeAI(model="gemini-pro",
+                             temperature=0.3)
+
+    prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
+    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+
+    return chain
+
+
+
+
+
+
+
+
+def main():
+    st.set_page_config("Chat PDF")
+    st.header("Chat with PDF using Gemini💁")
+
+    user_question = st.text_input("Ask a Question from the PDF Files")
+    pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
+
+      # Create the Chroma instance outside of the user_input() function
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    vector_store = Chroma(persist_directory="db", embedding_function=embeddings, collection_name="pdf_collection")
+    if st.button("Submit & Process"):
+            with st.spinner("Processing..."):
+                raw_text = get_pdf_text(pdf_docs)
+                text_chunks = get_text_chunks(raw_text)
+                get_vector_store(text_chunks)
+                st.success("Done")
+    if user_question:
+        user_input(vector_store,user_question)
+        
+def user_input(vector_store, user_question):
+    chain = get_conversational_chain()
+
+    docs = vector_store.similarity_search(user_question)
+
+    response = chain(
+        {"input_documents": docs, "question": user_question},
+        return_only_outputs=True,
+    )
+
+    print(response)
+    st.write("Reply: ", response["output_text"])
+
+     
+       
+
+
+
+if __name__ == "__main__":
+    main()
